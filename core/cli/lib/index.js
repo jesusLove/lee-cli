eaea25d4265fa5782df7dd5bc4314c637b39b5f5
lqq <jesus_lqq@163.com>
2021-06-10 22:09:13 +0800

    检查版本、检查Node版本、Root检查和降级

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -6,17 +6,51 @@ module.exports = core;
 // ? .js -> module.exports/exports
 // ? .json -> JSON.parse
 // * .any -> 当成 .js 解析
+
+const semver = require('semver')
+const colors = require('colors')
+
 const pkg = require('../package.json')
+const constant = require('./const')
 const log = require('@lee/log')
 
 function core() {
-  checkPkgVersion()
+  // try-catch 避免输入堆栈信息
+  try {
+    checkPkgVersion()
+    checkNodeVersion()
+    checkRoot()
+  } catch (e) {
+    log.error(e.message)
+  }
 }
 
+/**
+ * 权限降级，避免高级别身份创建的文件无法读写。
+ */
+function checkRoot() {
+  const rootCheck = require('root-check')
+  rootCheck()
+  log.info(process.geteuid())
+}
+
+/**
+ * 检查版本号
+ */
 function checkPkgVersion() {
-  console.log(pkg.version)
-
-  log()
+  log.info(pkg.version)
 }
-
+/**
+ * 检查 Node 版本号
+ */
+function checkNodeVersion() {
+  // * 获取当前版本号
+  const curVersion = process.version
+  // * 最低版本号
+  const lowestVersion = constant.LOWEST_NODE_VERSION
+  // * 对比版本号
+  if (!semver.gte(curVersion, lowestVersion)) {
+    throw new Error(colors.red(`lee 需要安装 v${lowestVersion} 以上版本的 Node.js`))
+  }
+}
 
--- utils/log/lib/index.js
+++ utils/log/lib/index.js
@@ -1,9 +1,13 @@
 'use strict';
 
-module.exports = index;
 
 const log = require('npmlog')
+// 判断 Debug 模式
+log.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'info'
+// 添加自定义
+log.heading = 'lee'
+log.headingStyle = { bg: 'green', fg: 'white', bold: true }
+// 自定义 log
+// log.addLevel('success', 2500, {fg: 'red'})
 
-function index() {
-  log.info('cli', 'test')
-}
+module.exports = log;
